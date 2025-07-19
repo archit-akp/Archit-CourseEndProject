65edce53f0003f11bb15c7428c16e649d08877f1
archit-akp <architmessi5@gmail.com>
2025-07-19 22:56:24 +0530

    third commit

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS Ecs";
+        return "DATA PAGE";
     }
 
     @GetMapping("/message")
     public String getMessage() {
-        return "Second message from AWS Ecs";
+        return "MESSAGE PAGE";
     }
 }
